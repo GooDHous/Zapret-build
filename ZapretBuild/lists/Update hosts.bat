e9e4e4ae79491dabc476044d48d34af9b6cd1c0e
GooDHous <165495399+GooDHous@users.noreply.github.com>
2024-11-13 21:25:46 +0700

    Add files via upload

--- ZapretBuild/lists/Update hosts.bat
+++ ZapretBuild/lists/Update hosts.bat
@@ -1,1 +1,9 @@
-curl -o hosts.txt  https://www.digitalocean.com/robots.txt
+@echo off
+@set a=Успешно
+cls
+title Updating hosts
+chcp 866
+color 2
+curl -o hosts.txt  https://raw.githubusercontent.com/GooDHous/HostsList/refs/heads/main/hosts.txt
+curl -o rules.txt https://raw.githubusercontent.com/GooDHous/HostsList/refs/heads/main/rules.txt
+echo %a%
