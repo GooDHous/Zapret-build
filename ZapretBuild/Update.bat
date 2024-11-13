ddf81e33c84434aae9c24fa5ce1f3b1491e17dd8
GooDHous <165495399+GooDHous@users.noreply.github.com>
2024-11-13 21:30:57 +0700

    Delete ZapretBuild directory

--- ZapretBuild/Update.bat
+++
@@ -1,2 +0,0 @@
-cd lists
-update.bat
--- ZapretBuild/lists/Update hosts.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-@set a=Успешно
-cls
-title Updating hosts
-chcp 866
-color 2
-curl -o hosts.txt  https://raw.githubusercontent.com/GooDHous/HostsList/refs/heads/main/hosts.txt
-curl -o rules.txt https://raw.githubusercontent.com/GooDHous/HostsList/refs/heads/main/rules.txt
-echo %a%
