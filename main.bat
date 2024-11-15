80f8c2256ded84cbacef726ba0857551bf61b41c
WehZB <vehovicveh@gmail.com>
2024-11-15 08:20:28 +0700

    Add files via upload

--- main.bat
+++ main.bat
@@ -5,7 +5,7 @@ cd ..
 cls
 cd /d "%~dp0"
 set BIN=%~dp0build\main-build\
-start "Build By DocFreeman and Weh" /min "%BIN%winws.exe" --wf-raw="@lists\rules.txt" ^
+start "Build By DocFreeman Chara and Weh" /min "%BIN%winws.exe" --wf-raw="@lists\rules.txt" ^
 --wf-tcp=80,443 --wf-udp=443,50000-65535 ^
 --filter-udp=443 --hostlist="lists\hosts.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-udplen-increment=10 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
 --filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
