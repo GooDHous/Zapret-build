c3daa03db44305373478f9a044bc6b32e6354673
Freeman <165495399+GooDHous@users.noreply.github.com>
2024-12-15 20:22:20 +0700

    Add files via upload

--- run.bat
+++ run.bat
@@ -7,7 +7,8 @@ echo Please wait: Updating hosts
 timeout /T 7 /NOBREAK > NUL
 cd /d "%~dp0"
 set BIN=%~dp0build\main-build\
-start "Zapret_Build By Weh, DocFreeman and Chara" /min "%BIN%winws.exe" --wf-raw="@lists\rules.txt" ^
+title Zapret_Build By Weh, DocFreeman and Chara (Build V1.5.2) 
+%BIN%\winws.exe --wf-raw="@lists\rules.txt" ^
 --wf-tcp=80,443 --wf-udp=443,50000-65535 ^
 --filter-udp=443 --hostlist="lists\hosts.txt" --methodeol --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-udplen-increment=10 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
 --filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
