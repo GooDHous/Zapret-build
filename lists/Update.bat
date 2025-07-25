d4469dba2af512afdea9cdf0b793f90b00a68707
GooDHous <165495399+GooDHous@users.noreply.github.com>
2025-07-25 22:33:17 +0300

    Delete lists/Update.bat

--- lists/Update.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-curl -o hosts.txt  https://raw.githubusercontent.com/GooDHous/HostsList/refs/heads/main/hosts.txt
-curl -o rules.txt https://raw.githubusercontent.com/GooDHous/HostsList/refs/heads/main/rules.txt
-exit
